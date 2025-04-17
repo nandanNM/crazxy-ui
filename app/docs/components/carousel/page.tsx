d831127cf4366c6704208f13f17bb59db0a549da
CoderNandan <nandanindia1947@gmail.com>
2025-04-17 21:42:31 +0530

    fix: correct image paths in Carousel component

--- app/docs/components/carousel/page.tsx
+++ app/docs/components/carousel/page.tsx
@@ -31,9 +31,9 @@ export default function CarouselPage() {
             <CraxyCarousel
               className="max-w-[400px]"
               images={[
-                "/carousell/image1.jpg",
-                "/carousell/image2.jpg",
-                "/carousell/image3.jpg",
+                "/carousel/image1.jpg",
+                "/carousel/image2.jpg",
+                "/carousel/image3.jpg",
               ]}
             />
           </div>
